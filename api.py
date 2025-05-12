df13c2cdf906bb6993e4189855fba509fc3264d9
“[Yatharth <[yatharthpatankar07@gmail.com]>
2025-05-12 08:29:06 +0530

    should take the first request using request function but done using postman. understood after again reading the question.

--- api.py
+++ api.py
@@ -1,22 +1,42 @@
 import requests
 
-url = "https://bfhldevapigw.healthrx.co.in/hiring/testWebhook/PYTHON"
-headers = {
-    "Authorization": "eyJhbGciOiJIUzI1NiJ9.eyJyZWdObyI6IjA4MjdBTDIyMTE1NSIsIm5hbWUiOiJZYXRoYXJ0aCBQYXRhbmthciIsImVtYWlsIjoieWF0aGFydGhwYXRhbmthcjIyMTA5N0BhY3JvcG9saXMuaW4iLCJzdWIiOiJ3ZWJob29rLXVzZXIiLCJpYXQiOjE3NDY5NjI2NjQsImV4cCI6MTc0Njk2MzU2NH0.G-leAoi2PMcSLe1lNOBLjgj1rQRujY-74Jn_qK3rRaA",
-    "Content-Type": "application/json"
+name = "Yatharth Patankar"
+reg_no = "0827AL221155"
+email = "yatharthpatankar221097@acropolis.in"
+
+url1 = "https://bfhldevapigw.healthrx.co.in/hiring/generateWebhook/PYTHON"
+body = {
+    "name": name,
+    "regNo": reg_no,
+    "email": email
 }
 
-data = {
-    "finalQuery": '''
-    SELECT p.AMOUNT AS SALARY,CONCAT(e.FIRST_NAME,' ',e.LAST_NAME) AS NAME,TIMESTAMPDIFF(YEAR,E.DOB,CURDATE()) AS AGE, d.DEPARTMENT_NAME FROM PAYMENTS p
-    JOIN EMPLOYEE e on p.EMP_ID = e.EMP_ID
-    JOIN DEPARTMENT d ON e.DEPARTMENT d ON e.DEPARTMENT=d.DEPARTMENT_ID
-    WHERE DAY(p.PAYMENT_TIME) !=1
-    ORDER BY p.AMOUNT DESC
-    LIMIT 1;
-'''
-}
+response1 = requests.post(url1, json=body)
 
-response = requests.post(url, json=data, headers=headers)
-print(response.status_code)
-print(response.text)
+if response1.status_code == 200:
+    data = response1.json()
+    webhook_url = data['webhook']
+    access_token = data['accessToken']
+
+    print("Webhook generated!")
+
+    url2 = webhook_url
+    headers = {
+        "Authorization": access_token,
+        "Content-Type": "application/json"
+    }
+
+    data = {
+        "finalQuery": '''
+        SELECT p.AMOUNT AS SALARY,CONCAT(e.FIRST_NAME,' ',e.LAST_NAME) AS NAME,TIMESTAMPDIFF(YEAR,E.DOB,CURDATE()) AS AGE, d.DEPARTMENT_NAME FROM PAYMENTS p
+        JOIN EMPLOYEE e on p.EMP_ID = e.EMP_ID
+        JOIN DEPARTMENT d ON e.DEPARTMENT d ON e.DEPARTMENT=d.DEPARTMENT_ID
+        WHERE DAY(p.PAYMENT_TIME) !=1
+        ORDER BY p.AMOUNT DESC
+        LIMIT 1;
+    '''
+    }
+
+    response2 = requests.post(url2, json=data, headers=headers)
+    print(response2.status_code)
+    print(response2.text)
